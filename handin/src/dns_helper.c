bd9b246b42cd21fb2026aca2b5404e612b5a19d4
meggie-wong <menggew@andrew.cmu.edu>
2019-05-01 01:37:39 -0400

    p3cp2 update grader

--- handin/src/dns_helper.c
+++ handin/src/dns_helper.c
@@ -78,7 +78,7 @@ void buffer_dns_header(char * buffer, dns_header_t* header) {
     uint16_t ptr = *(buffer + 2);
     uint16_t leftmask = 0b1100110011001100;
     uint16_t rightmask = 0x0011001100110011;
-    uint16_t tmp = ((ptr & leftmask) >> 2)) | ((ptr & rightmask) << 2);
+    uint16_t tmp = ((ptr & leftmask) >> 2) | ((ptr & rightmask) << 2);
     printf("%x, %x, %x\n", ptr, ptr & leftmask >> 2, ptr & rightmask << 2);
     memcpy(buffer+2, &tmp, 2);
 }
