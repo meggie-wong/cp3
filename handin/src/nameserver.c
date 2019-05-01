620e1862a647f1620343749a66c2d996310b6ffa
meggie-wong <menggew@andrew.cmu.edu>
2019-05-01 04:17:41 -0400

    p3cp2 update grader

--- handin/src/nameserver.c
+++ handin/src/nameserver.c
@@ -19,9 +19,9 @@ void get_query_name(query_message_t* query_message, char * query_name) {
 
 char* get_response_ip(char* query_name, char* client_ip) {
     char * response_ip;
-    printf("query name (len = %d) is %s\n", query_name, strlen(query_name));
-    // printf("record name (len = %d) is %s\n", dns_records.hostname, strlen(dns_records.hostname));
-    // printf("compare result = %d\n", strcmp(dns_records.hostname, query_name));
+    printf("query name (len = %d) is %s\n", strlen(query_name),query_name);
+    printf("record name (len = %d) is %s\n", strlen(dns_records.hostname, dns_records.hostname));
+    printf("compare result = %d\n", strcmp(dns_records.hostname, query_name));
     if ( method_robin ) {
         if( strcmp(dns_records.hostname, query_name) == 0 ) { // is video.cmu.cs.edu 
             response_ip = dns_records.server_ip[dns_records.resolve_cnt%dns_records.record_cnt];
